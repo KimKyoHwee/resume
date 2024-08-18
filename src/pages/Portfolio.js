fd74f5d53d1ba223300a36f39f42c6a4fedb364c
김교휘 <kimkyohwee@gimgyohwis-MacBook-Air.local>
2024-08-18 23:50:06 +0900

    feat: api 1번 연동완료, 테스트 완료

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Portfolio.css';
 import ppt1 from "../assets/ppt1.jpg";
 import ppt2 from "../assets/ppt2.jpg";
@@ -16,13 +16,14 @@ const images = [
 ];
 
 const Portfolio = () => {
+  const [loading, setLoading] = useState(false); // 로딩 상태 관리
 
   const handleButtonClick = async (index) => {
     if (index === 1) {
-      const choice = prompt('1번, 2번, 3번 중 선택해주세요:', '1');
+      const choice = prompt('1번(기존 방법), 2번(일반 Join), 3번(Fetch Join) 중 선택해주세요:', '1');
       if (choice === '1' || choice === '2' || choice === '3') {
         let threadCount = prompt('생성할 스레드 수를 입력하세요 (1~10):', '1');
-        let requestCount = prompt('각 스레드별로 보낼 요청 수를 입력하세요 (1~100):', '1');
+        let requestCount = prompt('각 스레드별로 보낼 요청 수를 입력하세요 (1~50):', '1');
 
         // 숫자 확인 및 제한 적용
         threadCount = parseInt(threadCount, 10);
@@ -33,10 +34,12 @@ const Portfolio = () => {
           return;
         }
 
-        if (isNaN(requestCount) || requestCount < 1 || requestCount > 100) {
+        if (isNaN(requestCount) || requestCount < 1 || requestCount > 50) {
           alert('요청 수는 1에서 100 사이의 숫자여야 합니다.');
           return;
         }
+
+        setLoading(true); // 로딩 상태 시작
 
         try {
           const response = await axios.get(`https://kyohwee.site/api/v1/mentor/performance/${choice}`, {
@@ -44,7 +47,7 @@ const Portfolio = () => {
               Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJjYXRlZ29yeSI6ImFjY2VzcyIsInVzZXJuYW1lIjoiZ29vZ2xlIDEwMTU2NzMyNTI3MTY1Mjk0OTQ2MyIsInJvbGUiOiJST0xFX01FTlRFRSIsImlhdCI6MTcyMzkxNTUzNCwiZXhwIjoxNzUzOTE1NTM0fQ.TakPICIU2fJ5f2zjnji4KSP6_qBXe0sg6fs2LzLWuTE`
             },
             params: {
-              apiNum: threadCount,
+              userCount: threadCount,
               totalRequests: requestCount,
             }
           });
@@ -58,6 +61,8 @@ const Portfolio = () => {
         } catch (error) {
           console.error(error);
           alert(`요청이 실패했습니다: ${error.message}`);
+        } finally {
+          setLoading(false); // 로딩 상태 종료
         }
         
       } else {
@@ -72,6 +77,7 @@ const Portfolio = () => {
 
   return (
     <div className="portfolio-content">
+      {loading && <div className="loading">Loading...</div>} {/* 로딩 메시지 */}
       {images.map((src, index) => (
         <section key={index} className="portfolio-section">
           <div className="portfolio-image-container">
@@ -79,7 +85,7 @@ const Portfolio = () => {
             {/* 왼쪽 상단의 버튼 (2, 3, 6 페이지에만 렌더링) */}
             {(index === 1 || index === 2 || index === 5) && (
               <button className="top-left-button" onClick={() => handleButtonClick(index)}>
-                Action Button
+                API 성능 측정하기
               </button>
             )}
             {/* 오른쪽 상단의 다음 페이지로 가는 버튼 */}
